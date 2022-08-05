904bcee262314743cccd9818525acf264037b92d
Sarah <rasdrive4165@protonmail.com>
2022-08-04 20:52:34 -0500

    added documentation to yes

--- src/yes/src/main.rs
+++ src/yes/src/main.rs
@@ -1,15 +1,27 @@
+///
+/// ## Name
+/// yes - output a string repeatedly until killed
+///
+/// ## Description
+/// The yes command outputs the same string, STRING, in a constant stream.
+/// If STRING is not specified, the word it repeats is y.
+///
+/// ## Arguments
+/// OPT `STRING` - the string to output
+///
+
 fn main() {
     let matches = clap::App::new("yes")
         .version("0.1")
         .author("Sarah Petkovic")
         .about("output a string repeatedly until killed")
-        .arg(clap::Arg::new("name").required(false))
+        .arg(clap::Arg::new("string").required(false))
         .get_matches();
 
     let data;
-    match matches.is_present("name") {
+    match matches.is_present("string") {
         true => {
-            data = matches.value_of("name").unwrap().to_string();
+            data = matches.value_of("string").unwrap().to_string();
         }
         false => {
             data = String::from("y");
