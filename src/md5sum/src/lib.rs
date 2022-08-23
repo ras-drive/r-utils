222bf41113fe3d1fe1dc08e54b11a629b0575a9d
Sarah <rasdrive4165@protonmail.com>
2022-08-23 15:25:46 -0500

    added proper file input support

--- src/md5sum/src/lib.rs
+++ src/md5sum/src/lib.rs
@@ -1,14 +1,46 @@
 use clap::ArgMatches;
 use md5::{compute, Digest};
-use std::io::BufRead;
+use std::{io::{BufRead, Read}, path::Path, fs::File};
 
-pub fn run(matches: ArgMatches) {
+pub fn run(matches: ArgMatches) -> anyhow::Result<()> {
     let filename = matches.value_of("filename").unwrap_or("-");
-    let input = match matches.is_present("filename") {
-        true => matches.value_of("filename").unwrap().to_string(),
+    let mut input: String = match matches.is_present("filename") {
+        true => {
+            let path = matches.value_of("filename").unwrap().to_string();
+            let path = Path::new(&path);
+            let display = path.display();
+            if Path::new(path).is_file() {
+                let mut file = match File::open(&path) {
+                    Err(why) => panic!("couldn't open {}: {}", display, why),
+                    Ok(file) => file,
+                };
+
+                // Read the file contents into a string, returns `io::Result<usize>`
+                let mut s = String::new();
+                match file.read_to_string(&mut s) {
+                    Err(why) => panic!("couldn't read {}: {}", display, why),
+                    Ok(_) => {
+                        s
+                    },
+                }
+            } else if path.is_dir() {
+                return Err(anyhow::Error::msg("cannot take a directory as input"));
+            } else {
+                return Err(anyhow::Error::msg("error, input is not a file"));
+            }
+
+
+        },
         false => std::io::stdin().lock().lines().next().unwrap().unwrap(),
     };
+
+    // TODO: check for -n flag to see if it should forego the newline
+    if !matches.is_present("filename") {
+        input.push('\n');
+    }
     println!("{:?} {}", checksum(&input), filename);
+
+    Ok(())
 }
 
 fn checksum(input: &String) -> Digest {
--- src/md5sum/src/main.rs
+++ src/md5sum/src/main.rs
@@ -4,11 +4,11 @@ use md5sum::run;
 
 fn main() {
     let matches = Command::new("md5sum")
-        .version("0.1")
+        .version("0.1.0")
         .author("Sarah Petkovic")
         .about("print or check md5 checksums")
         .arg(Arg::new("filename").required(false))
         .get_matches();
 
-    run(matches);
+    run(matches).unwrap();
 }
