4189fdbd7b481634833c5566eadd8b77de72c93c
Sarah <rasdrive4165@protonmail.com>
2022-08-23 00:46:12 -0500

    whoami command

---
+++ src/whoami/src/main.rs
@@ -0,0 +1,12 @@
+use clap::Command;
+use users::get_current_username;
+
+fn main() {
+    let _matches = Command::new("whoami")
+        .version("0.1.0")
+        .author("Sarah Petkovic")
+        .about("Print the user name associated with the current effective user ID.  Same as id -un.")
+        .get_matches();
+
+    println!("{}", get_current_username().unwrap().to_str().unwrap())
+}
